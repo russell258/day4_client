bbddc36d672de0d6d48bc011d92c995994a14cda
russell258 <duckie258@gmail.com>
2023-05-10 15:48:54 +0800

    client complete, can add more response for other commands or unexepcted command

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -5,7 +5,6 @@ import java.io.BufferedOutputStream;
 import java.io.Console;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -14,68 +13,40 @@ import java.net.UnknownHostException;
 
 public class App 
 {
-    public static void main( String[] args ) throws NumberFormatException, UnknownHostException, IOException
+    public static void main( String[] args ) throws UnknownHostException, IOException
     {
-        String serverHost = args[0];
-        String serverPort = args[1];
-
-        //establish connection to server - slide 8
-        // server must be started first
-        Socket socket = new Socket(serverHost, Integer.parseInt(serverPort));
-
-        //setup console input from keyboard
-        // variable to save keyboard inputs
-        //variable to save msgReceived
+        String portNumber = args[0];
+        String input = "";
         Console con = System.console();
-        String keyboardInput = "";
-        String msgReceived = "";
+        Socket socket = new Socket("localhost",Integer.parseInt(portNumber));
 
-
-
-        //similar to server - slide 9
-        try(InputStream is = socket.getInputStream()){
+        //send command to server write then read
+        try (OutputStream os = socket.getOutputStream()){
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            DataOutputStream dos = new DataOutputStream(bos);
+            
+            try (InputStream is = socket.getInputStream()){
             BufferedInputStream bis = new BufferedInputStream(is);
             DataInputStream dis = new DataInputStream(bis);
 
-            try(OutputStream os = socket.getOutputStream()){
-                BufferedOutputStream bos = new BufferedOutputStream(os);
-                DataOutputStream dos = new DataOutputStream(bos);
+            //while loop to send command
+            while (!input.equals("close")){
+                input = con.readLine("Enter a command: ");
+                dos.writeUTF(input);
+                dos.flush();
 
-                //while loop here
-                while(!keyboardInput.equals("close")){
-                    keyboardInput = con.readLine("Enter a command: ");
-
-                    //send message acrsos through the communication tunnel
-                    dos.writeUTF(keyboardInput);
-                    dos.flush();
-
-                    //receive message from server(response) and process it
-                    msgReceived = dis.readUTF();
-                    System.out.println(msgReceived);
-                }
-
-
-
-                // closes the output stream in reverse order
-
-                dos.close();
-                bos.close();
-                os.close();
-                
-
-            }catch (EOFException ex){
+                String msgReceived = dis.readUTF();
+                System.out.println(msgReceived);
 
             }
-            // closes the input stream in reverse order
+
+
             dis.close();
-            bis.close();
-            is.close();
-            socket.close();
-        }catch (EOFException ex){
-            ex.printStackTrace();
+            }
+
+            dos.close();
         }
-        
-      
- 
+        socket.close();
+
     }
 }
